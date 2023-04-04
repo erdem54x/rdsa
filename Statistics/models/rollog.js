51bd2470a82606581475e796e992cbb3e2fe8e0d
erdem54x <78647402+erdem54x@users.noreply.github.com>
2023-04-05 00:22:04 +0300

    Delete Statistics directory

--- Statistics/models/rollog.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require("mongoose");
-let Schema = mongoose.Schema;
-
-let roleLog = Schema({
-    user: String, 
-    roller: Array
-});
-
-module.exports = mongoose.model("rol-log", roleLog);
